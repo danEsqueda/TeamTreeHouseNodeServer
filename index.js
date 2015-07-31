b9d22a07d8c02f5459c3cebbe2ee58d94cf5756a
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-07-31 11:35:03 -0700

    adding routes

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  homeRoute(req, res);
 }).listen(3000);
 console.log('Server running');
+
+
+function homeRoute(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Header\n');
+  res.write('Search\n');
+  res.end('Footer\n');
+}
+
+function userRoute(req, res) {
+  var username = request.url.replace('/', '');
+  if (username.length > 0) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Header\n');
+    res.write(username + '\n');
+    res.end('Footer\n');
+  }
+}
